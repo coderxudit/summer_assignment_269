ff50f2cc4c8b0e11ad9017802734e871fb9bd406
Udit Patel <patelridit676@gmail.com>
2026-06-13 11:17:56 +0530

    Create Reverse Words in a String III.cpp

---
+++ week_2/day_4/Reverse Words in a String III.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        int n = s.length();
+        int i = 0;
+
+        while (i < n) {
+            int start = i;
+
+            while (i < n && s[i] != ' ') {
+                i++;
+            }
+
+            reverse(s.begin() + start, s.begin() + i);
+            i++; // skip space
+        }
+
+        return s;
+    }
+};
