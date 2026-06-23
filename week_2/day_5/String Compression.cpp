68c25d3c6d03f7ae15a434970e52f3fe75b6811d
Udit Patel <patelridit676@gmail.com>
2026-06-23 08:55:33 +0530

    Create String Compression.cpp

---
+++ week_2/day_5/String Compression.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        int i = 0;
+        int index = 0;
+
+        while (i < chars.size()) {
+            char currChar = chars[i];
+            int count = 0;
+
+            while (i < chars.size() && chars[i] == currChar) {
+                count++;
+                i++;
+            }
+
+            chars[index++] = currChar;
+
+            if (count > 1) {
+                string cnt = to_string(count);
+
+                for (char ch : cnt) {
+                    chars[index++] = ch;
+                }
+            }
+        }
+
+        return index;
+    }
+};
